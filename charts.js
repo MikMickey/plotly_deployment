bf0754bf49a2a78948a2569649a7bb300fe145e5
MikMickey <dixiemperalta@yahoo.com>
2021-06-06 20:57:13 -0700

    final module 12 challenge update

--- charts.js
+++ charts.js
@@ -138,70 +138,72 @@ function buildCharts(sample) {
 
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble",bubbleData, bubbleLayout); 
-  });
-}
+ 
 
 // Deliverable 3 Gauge Chart
 
 // 1. Create a variable that filters the metadata array for the object with the desired sample number.
-// var metadata = data.metadata;
-// var gaugeArray = metadata.filter(metaObj => metaObj.id == sample);  
+var metadata = data.metadata;
+var gaugeArray = metadata.filter(metaObj => metaObj.id == sample);  
     
-//     // Create a variable that holds the first sample in the array.
-//     var result = sampleArray[0];
-//     // 2. Create a variable that holds the first sample in the metadata array.
+    // Create a variable that holds the first sample in the array.
+    var result = sampleArray[0];
+    // 2. Create a variable that holds the first sample in the metadata array.
     
-//     var gaugeResult = gaugeArray[0];
-//     // Create variables that hold the otu_ids, otu_labels, and sample_values.
-//     var otu_ids = result.otu_ids
-//     var otu_labels = result.otu_labels //.slice(0, 10).reverse();
-//     var sample_values = result.sample_values //.slice(0,10).reverse();
+    var gaugeResult = gaugeArray[0];
+    // Create variables that hold the otu_ids, otu_labels, and sample_values.
+    var otu_ids = result.otu_ids
+    var otu_labels = result.otu_labels //.slice(0, 10).reverse();
+    var sample_values = result.sample_values //.slice(0,10).reverse();
 
-//     // 3. Create a variable that holds the washing frequency.
-//     var wfreqs = gaugeResult.wfreq;
-//     console.log(wfreqs)
-//     // Create the yticks for the bar chart.
-//     var yticks = otu_ids.map(sampleObj => "OTU " + sampleObj).slice(0,10).reverse();
+    // 3. Create a variable that holds the washing frequency.
+    var wfreqs = gaugeResult.wfreq;
+    console.log(wfreqs)
+    // Create the yticks for the bar chart.
+    var yticks = otu_ids.map(sampleObj => "OTU " + sampleObj).slice(0,10).reverse();
 
-//     // Use Plotly to plot the bar data and layout.
-//     Plotly.newPlot("bar",barData, barLayout);
+    // Use Plotly to plot the bar data and layout.
+    Plotly.newPlot("bar",barData, barLayout);
     
-//     // Use Plotly to plot the bubble data and layout.
-//     Plotly.newPlot("bubble",bubbleData, bubbleLayout); 
+    // Use Plotly to plot the bubble data and layout.
+    Plotly.newPlot("bubble",bubbleData, bubbleLayout); 
    
     
-//     // 4. Create the trace for the gauge chart.
-//     var gaugeData = [{
-//       value: wfreqs,
-//       type: "indicator",
-//       mode: "gauge+number",
-//       title: {text: "<b> Belly Button Washing Frequency </b> <br></br> Scrubs Per Week"},
-//       gauge: {
-//         axis: {range: [null,10], dtick: "2"},
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [{
+      value: wfreqs,
+      type: "indicator",
+      mode: "gauge+number",
+      title: {text: "<b> Belly Button Washing Frequency </b> <br></br> Scrubs Per Week"},
+      gauge: {
+        axis: {range: [null,10], dtick: "2"},
 
-//         bar: {color: "black"},
-//         steps:[
-//           {range: [0, 2], color: "red"},
-//           {range: [2, 4], color: "orange"},
-//           {range: [4, 6], color: "yellow"},
-//           {range: [6, 8], color: "lightgreen"},
-//           {range: [8, 10], color: "green"}
-//         ],
-//         dtick: 2
-//       }
-//     }];
+        bar: {color: "black"},
+        steps:[
+          {range: [0, 2], color: "red"},
+          {range: [2, 4], color: "orange"},
+          {range: [4, 6], color: "yellow"},
+          {range: [6, 8], color: "lightgreen"},
+          {range: [8, 10], color: "green"}
+        ],
+        dtick: 2
+      }
+    }];
       
              
     
 //     // 5. Create the layout for the gauge chart.
-//     var gaugeLayout = { 
-//       automargin: true
-//      };
+    var gaugeLayout = { 
+            width: 500, 
+           height: 500 
+           // automargin: true
+        
+     };
 
 //     // 6. Use Plotly to plot the gauge data and layout.
-//     Plotly.newPlot("gauge", gaugeData, gaugeLayout)
-//   });
-// }
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout)
+  });
+}
 
 
 
